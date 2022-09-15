4941185e09919ad553c237a869004630defcd248
IshtiaqueNafis <nafu22@gmail.com>
2022-09-15 13:57:36 -0400

    Initial commit

---
+++ src/main/java/com/example/gradcrud/GradCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradcrud/GradCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
